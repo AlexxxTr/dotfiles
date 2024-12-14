b2140da8eb2d9fe9c35f821a9312b02c5c03f239
Alexander Truyaert <alexander.truyaert@outlook.com>
2024-12-14 22:10:13 +0100

    add live grep function to telescope

--- .config/nvim/lua/alex/plugins/telescope.lua
+++ .config/nvim/lua/alex/plugins/telescope.lua
@@ -1,6 +1,65 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local make_entry = require("telescope.make_entry")
+local conf = require("telescope.config").values
+
+local live_mutligrep = function(opts)
+	opts = opts or {}
+	opts.cwd = opts.cwd or vim.uv.cwd()
+
+	local finder = finders.new_async_job({
+		command_generator = function(prompt)
+			if not prompt or prompt == "" then
+				return nil
+			end
+
+			local pieces = vim.split(prompt, "  ")
+			local args = { "rg" }
+
+			if pieces[1] then
+				table.insert(args, "-e")
+				table.insert(args, pieces[1])
+			end
+
+			if pieces[2] then
+				table.insert(args, "-g")
+				table.insert(args, pieces[2])
+			end
+
+			return vim.tbl_flatten({
+				args,
+				{
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--hidden",
+					"--no-ignore-vcs",
+				},
+			})
+		end,
+		entry_maker = make_entry.gen_from_vimgrep(opts),
+		cwd = opts.cwd,
+	})
+
+	pickers
+		.new(opts, {
+			debounce = 100,
+			prompt_title = "Multi grep",
+			finder = finder,
+			previewer = conf.grep_previewer(opts),
+			sorter = require("telescope.sorters").empty(),
+		})
+		:find()
+end
+
 return {
 	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 	opts = {},
 	keys = {
 		{
@@ -26,6 +85,10 @@ return {
 			function()
 				require("telescope.builtin").help_tags()
 			end,
+		},
+		{
+			"<leader>fg",
+			live_mutligrep,
 		},
 	},
 }
