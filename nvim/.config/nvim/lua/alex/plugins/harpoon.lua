1f7f867c773e0ed88df140dbd75a9a6e212f3386
Alexander Truyaert <alexander.truyaert@outlook.com>
2024-12-26 16:04:22 +0100

    Removed telescope dependency from harpoon
    I prefer the old version of the window, it's easier to edit

--- nvim/.config/nvim/lua/alex/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/alex/plugins/harpoon.lua
@@ -1,22 +1,3 @@
-local conf = require("telescope.config").values
-local function toggle_telescope(harpoon_files)
-	local file_paths = {}
-	for _, item in ipairs(harpoon_files.items) do
-		table.insert(file_paths, item.value)
-	end
-
-	require("telescope.pickers")
-		.new({}, {
-			prompt_title = "Harpoon",
-			finder = require("telescope.finders").new_table({
-				results = file_paths,
-			}),
-			previewer = conf.file_previewer({}),
-			sorter = conf.generic_sorter({}),
-		})
-		:find()
-end
-
 return {
 	"theprimeagen/harpoon",
 	branch = "harpoon2",
@@ -25,7 +6,7 @@ return {
 		harpoon:setup({})
 
 		vim.keymap.set("n", "<C-e>", function()
-			toggle_telescope(harpoon:list())
+			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end, { desc = "Open harpoon window" })
 
 		vim.keymap.set("n", "<leader>a", function()
