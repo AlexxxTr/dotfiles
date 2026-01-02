ee74d8e869909d3c295342157a41a896a28ccd72
Alexander Truyaert <alexander.truyaert@barco.com>
2026-01-02 14:40:37 +0100

    chore: make some updates to hyprland config and git integration

---
+++ nvim/.config/nvim/lua/alex/plugins/git.lua
@@ -0,0 +1,51 @@
+-- return {
+-- 	"tpope/vim-fugitive",
+-- 	command = { "G", "Git" },
+--     keys = {
+--         { "<leader>gs", vim.cmd.Git, desc = "Git Status" },
+--         { "<leader>gu", "<cmd>diffget //2<CR>", desc = "Git Diff Get Left" },
+--         { "<leader>gh", "<cmd>diffget //3<CR>", desc = "Git Diff Get Right" },
+--     },
+-- 	config = function()
+-- 		local fugitive_augroup = vim.api.nvim_create_augroup("fugitive_augroup", {})
+--
+-- 		vim.api.nvim_create_autocmd("BufWinEnter", {
+-- 			group = fugitive_augroup,
+-- 			pattern = "*",
+-- 			callback = function()
+-- 				if vim.bo.ft ~= "fugitive" then
+-- 					return
+-- 				end
+--
+-- 				local bufnr = vim.api.nvim_get_current_buf()
+-- 				local opts = { buffer = bufnr, remap = false }
+-- 				vim.keymap.set("n", "<leader>p", function()
+-- 					vim.cmd.Git("push")
+-- 				end, opts)
+--
+-- 				-- rebase always
+-- 				vim.keymap.set("n", "<leader>P", function()
+-- 					vim.cmd.Git("pull --rebase")
+-- 				end, opts)
+--
+-- 				-- NOTE: It allows me to easily set the branch i am pushing and any tracking
+-- 				-- needed if i did not set the branch up correctly
+-- 				vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+-- 			end,
+-- 		})
+-- 	end,
+-- }
+--
+return {
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"ibhagwan/fzf-lua", -- optional
+		},
+		cmd = "Neogit",
+		keys = {
+			{ "<leader>gs", "<cmd>Neogit kind=split_above<cr>", desc = "Show Neogit UI" },
+		},
+	},
+}
